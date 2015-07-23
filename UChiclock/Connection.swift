19cac8debdd8d25037d0856d401ad1839cbaa353
Alec Snyder <alec.snyder@originate.com>
2015-07-23 13:35:56 -0700

    Connect via https

--- UChiclock/Connection.swift
+++ UChiclock/Connection.swift
@@ -10,12 +10,13 @@ import Foundation
 
 func makeConnection()
 {
-    let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
-    let conn = NSURLSession(configuration : sessionConfig)
-    let targetURL = NSURL(string:"http://www.google.com")!
-    let task = conn.dataTaskWithURL(targetURL, completionHandler: dataHandler)
+    //let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+    let targetURL = NSURL(string:"https://uchicago.cybershift.net")!
+    let req = NSURLRequest(URL: targetURL);
+    //let URLConn = NSURLConnection(request: conn, delegate: nil);
+    let task = NSURLSession.sharedSession().dataTaskWithRequest(req, completionHandler: dataHandler)
     
-    task!.resume()
+    task.resume()
     
 }
 
@@ -23,11 +24,11 @@ func dataHandler( tup : (NSData?,NSURLResponse?, NSError?))
 {
     if(tup.2 != nil)
     {
-        print("error")
+        print("error: \(tup.2)")
         return
     }
     let (dat, _, _) = tup
     let response = NSString.init(data:dat!, encoding:NSUTF8StringEncoding)
     print("response is: \(response)")
-    
 }
+
